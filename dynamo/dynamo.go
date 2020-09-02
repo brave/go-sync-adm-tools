443766190c45ecfb7aebd26b9c84fb6c0cce39a7
Jocelyn Liu <yrliou@gmail.com>
2020-09-01 23:12:52 -0700

    Implement delete user data command

---
+++ dynamo/dynamo.go
@@ -0,0 +1,96 @@
+package dynamo
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+)
+
+const (
+	clientIDKey = "ClientID"
+	idKey       = "ID"
+	projPk      = clientIDKey + ", " + idKey
+	ttlAttrName = "TTL"
+)
+
+var (
+	// Can be modified during tests.
+	table = "client-entity"
+)
+
+// Dynamo is a Datastore wrapper around a dynamoDB.
+type Dynamo struct {
+	*dynamodb.DynamoDB
+}
+
+// DeleteUserData uses clientID to query all items under this clientID and set
+// ttl for them.
+func (dynamo *Dynamo) DeleteUserData(clientID string, ttl int64) error {
+	fmt.Println("Deleting user data for clientID", clientID)
+	// Query to get keys for all items under clientID as the partition key.
+	keyCond := expression.Key("ClientID").Equal(expression.Value(clientID))
+	exprs := expression.NewBuilder().WithKeyCondition(keyCond)
+	expr, err := exprs.Build()
+	if err != nil {
+		return fmt.Errorf("error building expression: %w", err)
+	}
+
+	input := &dynamodb.QueryInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		KeyConditionExpression:    expr.KeyCondition(),
+		ProjectionExpression:      aws.String(projPk),
+		TableName:                 aws.String(table),
+	}
+
+	var keys []map[string]*dynamodb.AttributeValue
+	err = dynamo.QueryPages(input,
+		func(queryOut *dynamodb.QueryOutput, last bool) bool {
+			keys = append(keys, queryOut.Items...)
+			return last
+		})
+	if err != nil {
+		return fmt.Errorf("error doing query: %w", err)
+	}
+
+	// For each key, update their ttl to 10 days after.
+	update := expression.Set(expression.Name(ttlAttrName), expression.Value(ttl))
+	expr, err = expression.NewBuilder().WithUpdate(update).Build()
+	if err != nil {
+		return fmt.Errorf("error building expression: %w", err)
+	}
+	for _, key := range keys {
+		input := &dynamodb.UpdateItemInput{
+			Key:                       key,
+			ExpressionAttributeNames:  expr.Names(),
+			ExpressionAttributeValues: expr.Values(),
+			UpdateExpression:          expr.Update(),
+			TableName:                 aws.String(table),
+		}
+		_, err := dynamo.UpdateItem(input)
+		if err != nil {
+			return fmt.Errorf("error updating ttl: %w", err)
+		}
+	}
+	fmt.Println("Successfully set ttl for", len(keys), "records")
+
+	return nil
+}
+
+// NewDynamo returns a dynamoDB client to be used.
+func NewDynamo() (*Dynamo, error) {
+	config := &aws.Config{
+		Region:   aws.String("us_west2"),
+		Endpoint: aws.String("http://localhost:8000"),
+	}
+	sess, err := session.NewSession(config)
+	if err != nil {
+		return nil, fmt.Errorf("error creating new AWS session: %w", err)
+	}
+
+	db := dynamodb.New(sess)
+	return &Dynamo{db}, nil
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/yrliou/go-sync-adm-tools/dynamo"
+)
+
+const (
+	ttl = time.Hour * 24 * 10 // 10 day
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("unsupported commands")
+		os.Exit(1)
+	}
+
+	db, err := dynamo.NewDynamo()
+	if err != nil {
+		fmt.Printf("Initialize dynamoDB session failed: %s\n", err.Error())
+	}
+	switch os.Args[1] {
+	case "delete":
+		if len(os.Args) < 3 {
+			fmt.Println("missing ClientID arg")
+			os.Exit(1)
+		}
+		err := db.DeleteUserData(os.Args[2], time.Now().Add(ttl).Unix())
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	default:
+		fmt.Println("unsupported commands")
+		os.Exit(1)
+	}
+}
